f38890af4b52e052add3651fdba4433bf4155dd1
rurfy <chrissy.richter@web.de>
2020-03-17 01:19:08 +0100

    TEST Action Greet

--- actions.py
+++ actions.py
@@ -7,19 +7,14 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-
+from rasa_sdk import Action
 
 class ActionGreetUser(Action):
 
     def name(self) -> Text:
         return "action_greet"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher, tracker, domain):
 
         dispatcher.utter_template("utter_greet", tracker)
 
